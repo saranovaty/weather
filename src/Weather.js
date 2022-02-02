336ba910b295bb37a6e8aea8d22bd0bd9121ec8b
saranovaty <sara.novati@runtastic.com>
2022-02-02 13:42:03 +0100

    Update Weather.js
    Added weather app

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,94 @@
 import React from "react";
-import axios from "axios";
+import "./Weather.css";
+import "./App.css";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
-  }
+export default function Weather() {
+  let weatherData = {
+    city: "Vienna",
+    temperature: 19,
+    date: "Tuesday",
+    hour: "10:00 a.m.",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    humidity: 80,
+    wind: 10,
+  };
 
-  let apiKey = "41bc8aa28e75257d10555fa1b7bee77d";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiURL).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  return (
+    <div>
+      <div className="card">
+        <div className="card-body">
+          <br />
+          <div className="row">
+            <div className="col">
+              <input
+                id="citta-nuova"
+                className="form-control"
+                type="text"
+                placeholder="Enter your city..."
+                autocomplete="off"
+              />
+            </div>
+            <div className="col-3">
+              <button type="submit" className="btn btn-warning cerca">
+                Search
+              </button>
+            </div>
+            <div className="col-3">
+              <input
+                type="button"
+                className="btn btn-warning current"
+                value="Current"
+              />
+            </div>
+          </div>
+          <div className="capital">{weatherData.city}</div>
+          <div className="day">
+            <span>{weatherData.date}</span>
+            <br />
+            <span>{weatherData.hour}</span>
+            <div className="condition">{weatherData.description}</div>
+          </div>
+          <div className="row">
+            <div className="col-4">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                id="icon"
+                className="nuvola"
+              />
+            </div>
+            <div className="col-4">
+              <div className="temperature">
+                10
+                <span className="celsius">
+                  <a href="#" className="active">
+                    °C
+                  </a>{" "}
+                  |
+                  <a href="#" className="active">
+                    °F
+                  </a>
+                </span>
+              </div>
+            </div>
+            <div className="col-4">
+              <div className="data">
+                <small className="info">
+                  <div>
+                    <strong>Humidity:</strong> {weatherData.humidity}%
+                  </div>
+                </small>
+                <small className="info">
+                  <div>
+                    <strong>Wind:</strong> {weatherData.wind}km/h
+                  </div>
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
