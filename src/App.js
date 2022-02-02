36331d3e0e33d8d489405106a8329367c038af2d
saranovaty <sara.novati@runtastic.com>
2022-02-02 13:41:56 +0100

    Update App.js
    Added weather app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Weather from "./Weather";
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Weather city="Tokyo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Weather />;
 }
 
 export default App;
